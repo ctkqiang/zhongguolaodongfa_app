f82e29601c6d5c7dabb5353d91563c3aa0885230
钟智强 <johnmelodymel@qq.com>
2023-12-21 02:29:32 +0800

    200

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -6,42 +6,12 @@ class InfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final item = [
-      "谁创建了此应用程序？",
-      "版本 1.0.0",
-    ];
-
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
       ),
-      body: ListView(
-        children: item.map((n) {
-          return ListTile(
-            title: Text(
-              n,
-              style: const TextStyle(
-                fontSize: 20,
-                fontFamily: "main",
-              ),
-            ),
-            onTap: () async {
-              final index = item.indexOf(n);
-
-              if (index == 0) {
-                Get.showSnackbar(
-                  const GetSnackBar(
-                    icon: Icon(Icons.info, color: Colors.green, size: 30),
-                    duration: Duration(seconds: 10),
-                    snackPosition: SnackPosition.TOP,
-                    message:
-                        "关注我的微信(@ctkqiang)， 钉钉(@chengtzekeong) 和 QQ(3072486255)",
-                  ),
-                );
-              }
-            },
-          );
-        }).toList(),
+      body: const Center(
+        child: Text("关注我的微信(@ctkqiang)"),
       ),
     );
   }
